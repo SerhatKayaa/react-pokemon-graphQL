093c33544f0be8ca20d19868a6d09ae27dfed43f
Kaya <serhat.kayaa3@gmail.com>
2020-02-26 06:59:33 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React  from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const App = () => {
+    return <div>sdfdsf</div>;
+};
+
+
+export default App;
